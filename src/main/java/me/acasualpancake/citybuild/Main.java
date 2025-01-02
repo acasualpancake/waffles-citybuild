9a7b1ea1697627c71546e72ce815e0a7fa2db77f
LostBoyHubble <malte-grossmann@gmx.de>
2025-01-02 15:54:32 +0100

    Added main class.

---
+++ src/main/java/me/acasualpancake/citybuild/Main.java
@@ -0,0 +1,20 @@
+package me.acasualpancake.citybuild;
+
+@SuppressWarnings("unused")
+public class Main {
+
+    public static void main(String[] args) {
+        GameContext gameContext = new GameContext();
+        addInternalPlugins();
+        addExternalPlugins();
+        gameContext.build();
+    }
+
+    public static void addInternalPlugins() {
+
+    }
+
+    public static void addExternalPlugins() {
+
+    }
+}
