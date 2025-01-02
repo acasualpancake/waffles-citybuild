e984a9b493550c5d64c958e5da4dee272a02c979
LostBoyHubble <malte-grossmann@gmx.de>
2025-01-02 15:54:18 +0100

    Added plugin and component build exceptions.

---
+++ src/main/java/me/acasualpancake/citybuild/engine/troubleshooting/CComponentException.java
@@ -0,0 +1,8 @@
+package me.acasualpancake.citybuild.engine.troubleshooting;
+
+@SuppressWarnings("unused")
+public class CComponentException extends RuntimeException {
+    public CComponentException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/me/acasualpancake/citybuild/engine/troubleshooting/PluginException.java
@@ -0,0 +1,8 @@
+package me.acasualpancake.citybuild.engine.troubleshooting;
+
+@SuppressWarnings("unused")
+public class PluginException extends RuntimeException {
+    public PluginException(String message) {
+        super(message);
+    }
+}
